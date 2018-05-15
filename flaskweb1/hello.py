acc8e94e6ec0ae500cb65941cd08013c5dfbee09
17680329677 <38883463+17680329677@users.noreply.github.com>
2018-05-15 21:59:06 +0800

    515版本数据库操作部分
    sqlite数据库操作

--- flaskweb1/hello.py
+++ flaskweb1/hello.py
@@ -1,17 +1,47 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import  datetime
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+basedir = os.path.abspath(os.path.dirname(__file__))    # 获取当前文件夹的（项目）的绝对路径
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "hard to guess string"
-manager = Manager(app)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')     # 程序时用的数据库URL必须保存到Flask配置对象的SQLALCHEMY_DATABASE_URI键中
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True      # 将SQLALCHEMY_COMMIT_ON_TEARDOWN设置为True，每次请求结束后都自动提交数据库的变动
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+manager = Manager(app)  # 此处不配置进入不了shell模式
 bootstrap = Bootstrap(app)
-moment = Moment(app)
+moment = Moment(app)    # 时间处理
+db = SQLAlchemy(app)      # 数据库
+
+
+# 定义模型
+class Role(db.Model):
+    __tablename__ = 'roles'     # 定义表名称
+    id = db.Column(db.Integer, primary_key=True)    # 定义id列属性为整型，主键
+    name = db.Column(db.String(64), unique=True)    # 定义name属性为6长度64的字符串 不可重复
+    users = db.relationship('User', backref='role')     # users属性返回与角色关联的用户组成的列表 backref向User模型中添加一个role属性，从而半箱定义关系
+
+    def __repr__(self):
+        return '<Role %r>' % self.name       # 该方法返回一个具有可读性的字符串表示模型
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)    # index设置为True 为这列设置索引提高查询
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 # 定义表单类
